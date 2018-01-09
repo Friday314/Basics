af314862efb95862e3fa14f1ba2dee722b74d13a
Friday314 <zhangzheng@email.com>
2018-01-09 20:30:50 +0800

    Commit

---
+++ zz_00_笔记本.py
@@ -0,0 +1,10 @@
+# String相关
+
+_str = "Python is My Love"
+print("_str 原文本：%s" % _str)
+
+# split() 通过给定的字符，将字符串分割为一个列表
+print("split()方法：" + _str.split(" "))
+
+# replace() 类似文本中的替换查找，将给定的字符替换成另外一个字符
+print("replace()方法：" + _str.replace(" ", "_"))
