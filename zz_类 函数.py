5972b1828279dd182c76998aba3141dfe57658eb
Friday314 <zhangzheng@email.com>
2018-01-11 16:50:50 +0800

    Commit

--- zz_类 函数.py
+++ zz_类 函数.py
@@ -1,6 +1,11 @@
+# 定义类
 class Bike:
 
     compose = ['frame', 'wheel', 'pedal']
+
+    # 定义方法
+    def use(self):
+        print("you are riding")
 
 
 
@@ -9,3 +14,5 @@ you_bike = Bike()
 
 print(my_bike.compose)
 print(you_bike.compose)
+
+my_bike.use()
