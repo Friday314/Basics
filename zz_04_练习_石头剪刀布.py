4921a305d44b8d07a12c742c213ddc2422a28dca
Friday314 <zhangzheng@email.com>
2018-01-12 16:35:34 +0800

    Commit

---
+++ zz_04_练习_石头剪刀布.py
@@ -0,0 +1,38 @@
+
+import random
+
+
+def rock_paper_scissors():
+
+    """
+    石头剪刀布游戏
+    """
+
+    while True:
+
+        rad = random.randint(0, 2)
+
+        number = int(input("请输入：剪刀(0)  石头(1)  布(2)："))
+
+        if number not in range(0, 3):
+            print(rad)
+            print("输入错误，请重新输入！")
+
+        elif rad == 0 and number == 2:
+            print(rad)
+            print("输了，不要走，决战到天亮！")
+
+        elif rad == 1 and number == 0:
+            print(rad)
+            print("输了，不要走，决战到天亮！")
+
+        elif rad == 2 and number == 0:
+            print(rad)
+            print("输了，不要走，决战到天亮！")
+
+        else:
+            print(rad)
+            print("恭喜你！取得胜利！")
+
+
+rock_paper_scissors()
