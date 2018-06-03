80624239fb7be6b7934b462fa58132c4fcd116a5
ZhangZheng <35223774+Friday314@users.noreply.github.com>
2018-06-03 22:49:30 +0800

    Commit

--- zz_06_类 函数.py
+++ zz_06_类 函数.py
@@ -16,3 +16,27 @@ print(my_bike.compose)
 print(you_bike.compose)
 
 my_bike.use()
+
+
+# 定义一个全局变量
+wendu = 33
+
+
+def get_wendu():
+
+    # 定义的局部变量
+    # wendu = 30
+
+    """
+    如果：全局变量与局部变量重名，在方法中调用时，默认调用局部变量
+    """
+
+    # 使用global用来声明一个变量，那么就是对全局变量来进行修改
+    global wendu
+
+    wendu = 40
+
+    print(wendu)
+
+
+get_wendu()
