4dd1ca39749dd0fe7b981a4d03eb9e30102c4f2d
ZhangZheng <35223774+Friday314@users.noreply.github.com>
2018-06-11 21:45:48 +0800

    Commit

--- zz_06_类 函数.py
+++ zz_06_类 函数.py
@@ -53,3 +53,48 @@ text(10)
 
 # 不使用缺省参数
 text(11,22)
+
+
+# 不定长参数,*args,代表一个元祖。如果方法有多个参数，元祖需要放在最后一个参数
+def number(a, b, *args):
+    print(a)
+    print(b)
+    print(args)
+
+    sum = a + b
+
+    for num in args:
+        sum += num
+
+    print(sum)
+
+
+number(1, 2, 3, 4, 5, 6, 7, 8, 9)
+
+
+# 不定长参数，*表示：可以保存多余的，不定长的参数。**表示：可以保存实参里带有变量名的参数
+def number_2(a, b, *args, **kwargs):
+    print(a)
+    print(b)
+    print(args)
+    print(kwargs)
+
+
+number_2(11, 22, 33, A = 11, B = 22)
+
+
+# 拆包
+def number_3(a, b, *args, **kwargs):
+    print(a)
+    print(b)
+    print(args)
+    print(kwargs)
+
+
+A = (33, 44, 55)
+B = {"name":"laowang", "age":18}
+
+number_3(11, 22, A, B)
+
+# 拆包调用
+number_3(11, 22, *A, **B)
---
+++ zz_08_可变_不可变类型.py
@@ -0,0 +1,31 @@
+# 不可变类型
+
+# 数字类型
+print("A")
+
+A = 100
+print(id(A))
+
+A = 101
+print(id(A))
+
+# 字符串类型
+print("B")
+
+B = "HelloWorld"
+print(id(B))
+
+B = "HelloWord"
+print(id(B))
+
+# 元祖类型
+
+
+print("-----" * 10)
+
+
+# 可变类型
+
+# 列表类型
+
+# 字典类型
