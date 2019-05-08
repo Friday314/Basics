731a5a877cee8ce56bd8708b6c7d0def60941a86
ZHANGZHENG <zhangzheng@email.com>
2019-05-08 12:44:35 +0800

    Commit

---
+++ Basics_01_数据类型.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2019-05-08 10:12:02
+# @Author  : Zheng Zhang (zhangzheng@email.com)
+# @Link    : 
+# @Version : 1.0
+
+import os
+
+"""
+
+                int(有符号整数)
+
+                long(长整型【也可以代表八进制和十六进制】)
+Number (数字)
+                float(浮点型)
+
+                complex(复数)
+
+
+                True
+布尔类型
+                False
+
+
+String (字符串类型)
+
+
+List (列表)
+
+
+Tuple (元组)
+
+
+Dictionary (字典)
+
+"""
+
+_number = 100
+
+_str = "Hello Word"
+
+_bool = True
+
+_list = [1, 2, 3, 4, 5]
+
+_tup = (1, 2, 3, 4, 5)
+
+_dict = {"Name": "ZhangSan",
+         "Age": 18}
+
+# type() 输出变量的数据类型
+
+print(type(_number))
+print(_number)
+
+print("----------------")
+
+print(type(_str))
+print(_str)
+
+print("----------------")
+
+print(type(_bool))
+print(_bool)
+
+print("----------------")
+
+print(type(_list))
+print(_list)
+
+print("----------------")
+
+print(type(_tup))
+print(_tup)
+
+print("----------------")
+
+print(type(_dict))
+print(_dict)
