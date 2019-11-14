bea69bdf60bb4e6cfaee3bcf2222c42355dc9d1c
张正 <zhangzheng@email.com>
2019-11-14 17:14:11 +0800

    修改内存

--- Basics_17_OS 窗体控制.py
+++ Basics_17_OS 窗体控制.py
@@ -1,4 +1,6 @@
 import os
+import time
+
 import win32con
 
 '''
@@ -6,3 +8,17 @@ import win32con
 目前安装pypiwin32,支持导入win32gui
 '''
 import win32gui
+
+# 获取窗口句柄句,参数（窗体类名,窗口标题）
+微信 = win32gui.FindWindow("TrayNotifyWnd", "微信")
+
+print(微信)
+
+# 隐藏窗口
+win32gui.ShowWindow(微信, win32con.SW_HIDE)
+
+# 延时任务，秒
+time.sleep(2)
+
+# 显示窗体
+win32gui.ShowWindow(微信, win32con.SW_SHOW)
