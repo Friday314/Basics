ac2a17321bb086ed22cd167a88387276475ea45e
张正 <zhangzheng@email.com>
2019-11-25 16:00:11 +0800

    递归显示系统文件目录

--- Basics_19_递归.py
+++ Basics_19_递归.py
@@ -17,3 +17,32 @@ def getNums(num):
 
 
 print(getNums(4))
+
+# 递归遍历目录
+
+import os
+
+
+def getAllDir(path):
+    # 得到当前目录下所有文件
+    filesList = os.listdir(path)
+
+    print(filesList)
+
+    # 处理每一个文件
+    for fileName in filesList:
+
+        # 判断是否是文件夹或者文件(要用绝对路径)
+        fileAbsPath = os.path.join(path, fileName)
+
+        if os.path.isdir(fileAbsPath):
+
+            print("目录：", fileName)
+
+            getAllDir(fileAbsPath)
+
+        else:
+            print("普通文件：", fileName)
+
+
+getAllDir(r"D:\Cache")
